ea6962d04128332d4591d752d65007e0d09fdfc4
楼力栋 <17342046+lou-lidong@user.noreply.gitee.com>
2026-08-06 10:32:04 +0800

    审批流程明细prd

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const version="2.2.575-construction-log-multi-image-upload";
+  const version="2.2.576-approval-flow-prd";
   const scripts=[
     "src/app/core/00-data-and-utils.js",
     "src/vendor/jszip.min.js",
