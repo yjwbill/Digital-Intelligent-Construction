6f9810f6dae1c73548bd53e6f28a223790b21338
yjw <yjwbill@qq.com>
2026-08-14 18:35:06 +0800

    新增 企业端和项目端设备管理模块

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  const version="2.2.679-project-button-permission-and-edit-form";
+  const version="2.2.699-enterprise-equipment-filter-bar-fields";
   const scripts=[
     "src/app/core/00-data-and-utils.js",
     "src/vendor/jszip.min.js",
